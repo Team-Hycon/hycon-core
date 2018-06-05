e055674e1560502c5c86ce03dece215ad5f532e1
Niall Moore <niall@Nialls-iMac.local>
2018-06-05 09:05:47 +0900

    Restored wallet

---
+++ src/wallet/mnemonic.ts
@@ -0,0 +1,16 @@
+import { chinese_simplified } from "../../mnemonic/chinese_simplified"
+import { chinese_traditional } from "../../mnemonic/chinese_traditional"
+import { english } from "../../mnemonic/english"
+import { french } from "../../mnemonic/french"
+import { italian } from "../../mnemonic/italian"
+import { japanese } from "../../mnemonic/japanese"
+import { korean } from "../../mnemonic/korean"
+import { spanish } from "../../mnemonic/spanish"
+export const CHINESE_SIMPLIFIED_WORDLIST = chinese_simplified
+export const CHINESE_TRADITIONAL_WORDLIST = chinese_traditional
+export const ENGLISH_WORDLIST = english
+export const FRENCH_WORDLIST = french
+export const ITALIAN_WORDLIST = italian
+export const JAPANESE_WORDLIST = japanese
+export const KOREAN_WORDLIST = korean
+export const SPANISH_WORDLIST = spanish
